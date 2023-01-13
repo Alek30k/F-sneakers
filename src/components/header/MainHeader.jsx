c7e5d6149a86beaac33913f7834043a048deca06
Alek30k <alemank23k@gmail.com>
2023-01-13 18:38:44 -0300

    last commit

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -6,7 +6,7 @@ import CartIcon from "@/components/icons/CartIcon";
 
 const MainHeader = () => {
   return (
-    <header className="flex">
+    <header className="container mx-auto flex bg-slate-400 px-4">
       <button className="md:hidden">
         <MenuIcon />
       </button>
@@ -15,18 +15,18 @@ const MainHeader = () => {
         alt="Logo sneakers"
         className="mr-auto mb-1 h-5 md:mr-0"
       />
-      <nav className="hidden md:block">
+      <nav className="hidden md:mr-auto md:block">
         <a href="">Collections</a>
         <a href="">Men</a>
         <a href="">Women</a>
         <a href="">About</a>
         <a href="">Contact</a>
       </nav>
-      <div className="flex">
+      <div className="flex gap-4">
         <button>
           <CartIcon />
         </button>
-        <img src={AvatarImage} alt="" />
+        <img src={AvatarImage} alt="" className="w-10" />
       </div>
     </header>
   );
