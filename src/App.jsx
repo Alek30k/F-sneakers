f33b6b14257a6f0ae861a91ad259e5de39019014
Alek30k <alemank23k@gmail.com>
2023-01-13 13:59:38 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,16 @@ const App = () => {
         <section>
           <p>Sneaker Company</p>
           <h2>Fall Limit Edition Sneakers</h2>
+          <p>
+            "These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they’ll withstand everything
+            the weather can offer.",
+          </p>
+          <p>
+            <span>$125.00</span>
+            <span>50%</span>
+          </p>
+          <p>$250.00</p>
         </section>
       </main>
     </>
