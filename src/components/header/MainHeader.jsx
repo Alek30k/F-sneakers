59c74b0de8708ad5c750bd4571154288ea4ff12f
Alek30k <alemank23k@gmail.com>
2023-01-14 10:53:26 -0300

    last commit

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -3,6 +3,7 @@ import LogoSneakes from "@/assets/images/logo.svg";
 import AvatarImage from "@/assets/images/image-avatar.png";
 import MenuIcon from "@/components/icons/MenuIcon";
 import CartIcon from "@/components/icons/CartIcon";
+import CloseIcon from "../icons/CloseIcon";
 
 const MainHeader = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
@@ -15,7 +16,7 @@ const MainHeader = () => {
   };
 
   return (
-    <header className="container relative mx-auto flex  items-center gap-8 bg-gray-100 px-4 py-8">
+    <header className="container mx-auto flex  items-center gap-8 bg-gray-100 px-4 py-8">
       <button className="md:hidden" onClick={handleOpenMenu}>
         <MenuIcon />
       </button>
@@ -24,7 +25,10 @@ const MainHeader = () => {
         alt="Logo sneakers"
         className="mr-auto mb-1 h-5 md:mr-0"
       />
-      <nav className="absolute top-0 left-0 flex flex-col bg-gray-400 md:mr-auto md:flex md:flex-row md:gap-4 ">
+      <nav className="absolute top-0 left-0 flex h-full w-4/5 flex-col gap-y-5 bg-white p-8 font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:p-0">
+        <button className="mb-12 md:hidden" onClick={handleCloseMenu}>
+          <CloseIcon />
+        </button>
         <a href="">Collections</a>
         <a href="">Men</a>
         <a href="">Women</a>
