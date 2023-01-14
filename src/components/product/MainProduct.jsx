f1b74ceba00aa6cc230bc99d2acfd7edb931185a
Alek30k <alemank23k@gmail.com>
2023-01-14 16:40:44 -0300

    last commit

--- src/components/product/MainProduct.jsx
+++ src/components/product/MainProduct.jsx
@@ -1,31 +1,12 @@
 import React from "react";
+import DetailsProduct from "@/components/product/details/DetailsProduct";
+import SlideProduct from "@/components/product/gallery/SlideProduct";
 
 const MainProduct = () => {
   return (
-    <main>
-      <section>galeria de imagenes</section>
-      <section>
-        <p>Sneaker Company</p>
-        <h2>Fall Limit Edition Sneakers</h2>
-        <p>
-          "These low-profile sneakers are your perfect casual wear companion.
-          Featuring a durable rubber outer sole, they’ll withstand everything
-          the weather can offer.",
-        </p>
-        <p>
-          <span>$125.00</span>
-          <span>50%</span>
-        </p>
-        <p>$250.00</p>
-        <div>
-          <div>
-            <button>-</button>
-            <span>0</span>
-            <button>+</button>
-          </div>
-          <button>Add to cart</button>
-        </div>
-      </section>
+    <main className="grid grid-cols-1 gap-8 md:grid-cols-2">
+      <SlideProduct />
+      <DetailsProduct />
     </main>
   );
 };
---
+++ src/components/product/gallery/SlideProduct.jsx
@@ -0,0 +1,13 @@
+export default () => {
+  return (
+    <section className="grid md:grid-cols-4">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </section>
+  );
+};
