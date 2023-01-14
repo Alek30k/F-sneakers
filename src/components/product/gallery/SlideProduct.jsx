6bbd73027751501d4598a45a7b4f6d5e924f8edb
Alek30k <alemank23k@gmail.com>
2023-01-14 16:51:04 -0300

    last commit

--- src/components/product/gallery/SlideProduct.jsx
+++ src/components/product/gallery/SlideProduct.jsx
@@ -1,13 +1,20 @@
+import imgProduct1 from "@/assets/images/image-product-1.jpg";
+
+import imgProductSmall1 from "@/assets/images/image-product-1-thumbnail.jpg";
+import imgProductSmall2 from "@/assets/images/image-product-2-thumbnail.jpg";
+import imgProductSmall3 from "@/assets/images/image-product-3-thumbnail.jpg";
+import imgProductSmall4 from "@/assets/images/image-product-4-thumbnail.jpg";
+
 export default () => {
   return (
     <section className="grid md:grid-cols-4">
-      <div>
-        <img src="" alt="" />
+      <div className="col-span-4">
+        <img src={imgProduct1} alt="" />
       </div>
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
+      <img src={imgProductSmall1} alt="" />
+      <img src={imgProductSmall2} alt="" />
+      <img src={imgProductSmall3} alt="" />
+      <img src={imgProductSmall4} alt="" />
     </section>
   );
 };
