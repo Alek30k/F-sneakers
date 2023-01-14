b1ff3ae9d341223957749bb251b538bdef38c7f9
Alek30k <alemank23k@gmail.com>
2023-01-14 15:00:44 -0300

    last commit

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -3,7 +3,8 @@ import LogoSneakes from "@/assets/images/logo.svg";
 import AvatarImage from "@/assets/images/image-avatar.png";
 import MenuIcon from "@/components/icons/MenuIcon";
 import CartIcon from "@/components/icons/CartIcon";
-import CloseIcon from "../icons/CloseIcon";
+import CloseIcon from "@/components/icons/CloseIcon";
+import NavLinkHeader from "@/components/header/NavLinkHeader";
 
 const MainHeader = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
@@ -17,7 +18,7 @@ const MainHeader = () => {
 
   return (
     <>
-      <header className="container mx-auto flex  items-center gap-8 py-8">
+      <header className="container mx-auto flex  items-center gap-8">
         <button className="md:hidden" onClick={handleOpenMenu}>
           <MenuIcon />
         </button>
@@ -36,11 +37,11 @@ const MainHeader = () => {
           <button className="mb-12 md:hidden" onClick={handleCloseMenu}>
             <CloseIcon />
           </button>
-          <a href="">Collections</a>
-          <a href="">Men</a>
-          <a href="">Women</a>
-          <a href="">About</a>
-          <a href="">Contact</a>
+          <NavLinkHeader text="Collections" />
+          <NavLinkHeader text="Men" />
+          <NavLinkHeader text="Women" />
+          <NavLinkHeader text="About" />
+          <NavLinkHeader text="Contact" />
         </nav>
         <div className="flex gap-4 ">
           <button>
