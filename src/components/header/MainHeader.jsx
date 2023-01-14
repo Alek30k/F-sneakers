a14304690e878a7741bd311b7a3068574d3e9df6
Alek30k <alemank23k@gmail.com>
2023-01-14 09:46:55 -0300

    last commit

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -15,7 +15,7 @@ const MainHeader = () => {
   };
 
   return (
-    <header className="container mx-auto flex  items-center gap-8 bg-gray-400 px-4 py-8">
+    <header className="container relative mx-auto flex  items-center gap-8 bg-gray-100 px-4 py-8">
       <button className="md:hidden" onClick={handleOpenMenu}>
         <MenuIcon />
       </button>
@@ -24,7 +24,7 @@ const MainHeader = () => {
         alt="Logo sneakers"
         className="mr-auto mb-1 h-5 md:mr-0"
       />
-      <nav className="absolute flex flex-col md:mr-auto md:flex md:flex-row md:gap-4 ">
+      <nav className="absolute top-0 left-0 flex flex-col bg-gray-400 md:mr-auto md:flex md:flex-row md:gap-4 ">
         <a href="">Collections</a>
         <a href="">Men</a>
         <a href="">Women</a>
