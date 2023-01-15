17ec519129778d6f461ef00ac4dac4e13db9d791
Alek30k <alemank23k@gmail.com>
2023-01-15 10:23:16 -0300

    last commit

--- src/components/product/gallery/SlideProduct.jsx
+++ src/components/product/gallery/SlideProduct.jsx
@@ -1,22 +1,46 @@
-import imgProduct1 from "@/assets/images/image-product-1.jpg";
 import PrevIcon from "@/components/icons/PrevIcon";
 import NextIcon from "@/components/icons/NextIcon";
+
+import imgProduct1 from "@/assets/images/image-product-1.jpg";
+import imgProduct2 from "@/assets/images/image-product-2.jpg";
+import imgProduct3 from "@/assets/images/image-product-3.jpg";
+import imgProduct4 from "@/assets/images/image-product-4.jpg";
 
 import imgProductSmall1 from "@/assets/images/image-product-1-thumbnail.jpg";
 import imgProductSmall2 from "@/assets/images/image-product-2-thumbnail.jpg";
 import imgProductSmall3 from "@/assets/images/image-product-3-thumbnail.jpg";
 import imgProductSmall4 from "@/assets/images/image-product-4-thumbnail.jpg";
+import { useState } from "react";
+
+const ARRAY_IMGS = [imgProduct1, imgProduct2, imgProduct3, imgProduct4];
 
 export default () => {
+  const [index, setIndex] = useState(0);
+
+  const handleClickNext = () => {
+    setIndex(index + 1);
+    // index === ARRAY_IMGS.length - 1 ? setIndex(0) : setIndex(index + 1);
+  };
+  const handleClickPrev = () => {
+    console.log("first");
+    // index === 0 ? setIndex(ARRAY_IMGS.length - 1) : setIndex(index - 1);
+  };
+
   return (
     <section className="grid md:grid-cols-4 md:gap-4">
       <div className="relative col-span-4">
-        <img src={imgProduct1} alt="" className="aspect-[16/12]" />
-        <div className="absolute top-1/2 left-0">
-          <button className="grid h-10 w-10 place-items-center  rounded-full bg-white">
+        <img src={ARRAY_IMGS[index]} alt="" className="aspect-[16/12]" />
+        <div className="absolute top-1/2 left-0 flex w-full -translate-y-1/2 justify-between px-5">
+          <button
+            className="grid h-10 w-10 place-items-center  rounded-full bg-white"
+            onClick={handleClickPrev}
+          >
             <PrevIcon />
           </button>
-          <button className=" grid h-10 w-10 place-items-center rounded-full bg-white">
+          <button
+            className=" grid h-10 w-10 place-items-center rounded-full bg-white"
+            onClick={handleClickNext}
+          >
             <NextIcon />
           </button>
         </div>
