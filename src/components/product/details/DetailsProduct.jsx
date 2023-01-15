d43a3811100d2afb535f38ecea364db62af676d5
Alek30k <alemank23k@gmail.com>
2023-01-15 16:35:11 -0300

    last commit

--- src/components/product/details/DetailsProduct.jsx
+++ src/components/product/details/DetailsProduct.jsx
@@ -1,18 +1,20 @@
 export default () => {
   return (
-    <section>
-      <p>Sneaker Company</p>
-      <h2>Fall Limit Edition Sneakers</h2>
-      <p>
+    <section className="container mx-auto px-6">
+      <p className="mb-3 font-bold uppercase tracking-wide text-orange-primary">
+        Sneaker Company
+      </p>
+      <h2 className="mb-4 text-3xl font-bold ">Fall Limit Edition Sneakers</h2>
+      <p className="mb-5 text-dark-grayish-blue">
         "These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they’ll withstand everything the
         weather can offer.",
       </p>
-      <p>
-        <span>$125.00</span>
+      <div className="grid grid-cols-3 font-bold">
+        <span className="text-3xl">$125.00</span>
         <span>50%</span>
-      </p>
-      <p>$250.00</p>
+        <span>$250.00</span>
+      </div>
       <div>
         <div>
           <button>-</button>
