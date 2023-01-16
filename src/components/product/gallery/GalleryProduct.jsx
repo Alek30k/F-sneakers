b847bcd7e86c36beb5d0c46c3d80e02ad941e999
Alek30k <alemank23k@gmail.com>
2023-01-16 16:53:16 -0300

    last commit

--- src/components/product/gallery/GalleryProduct.jsx
+++ src/components/product/gallery/GalleryProduct.jsx
@@ -1,6 +1,8 @@
 import SlideProduct from "@/components/product/gallery/SlideProduct";
+import { useState } from "react";
 
 export default ({ ARRAY_IMGS, ARRAY_IMG_SMALL }) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
     <>
       <SlideProduct
@@ -8,11 +10,15 @@ export default ({ ARRAY_IMGS, ARRAY_IMG_SMALL }) => {
         ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
         className="grid md:grid-cols-4 md:gap-4 "
       />
-      <SlideProduct
-        ARRAY_IMGS={ARRAY_IMGS}
-        ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
-        className="md:mx-w-xl grid md:absolute md:top-1/2 md:left-1/2 md:-translate-y-1/2 md:-translate-x-1/2 md:grid-cols-4 md:gap-4"
-      />
+      {isOpenModal && (
+        <>
+          <SlideProduct
+            ARRAY_IMGS={ARRAY_IMGS}
+            ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
+            className="md:mx-w-xl grid md:absolute md:top-1/2 md:left-1/2 md:-translate-y-1/2 md:-translate-x-1/2 md:grid-cols-4 md:gap-4"
+          />
+        </>
+      )}
     </>
   );
 };
