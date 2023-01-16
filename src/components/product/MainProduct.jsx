df218871e0f2810a83cb9631c8daddf61f80f8e8
Alek30k <alemank23k@gmail.com>
2023-01-16 00:01:46 -0300

    last commit

--- src/components/product/MainProduct.jsx
+++ src/components/product/MainProduct.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import DetailsProduct from "@/components/product/details/DetailsProduct";
+import GalleryProduct from "@/components/gallery/GalleryProduct";
 
 const MainProduct = () => {
   return (
     <main className="grid grid-cols-1 items-center gap-8 md:container md:mx-auto md:min-h-[calc(100vh-88px-3px)] md:grid-cols-2">
+      <GalleryProduct />
       <DetailsProduct />
     </main>
   );
