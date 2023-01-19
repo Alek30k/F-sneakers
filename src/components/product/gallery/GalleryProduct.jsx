5d8776c022dc3be98611dbc416a44eda5cc0ca47
Alek30k <alemank23k@gmail.com>
2023-01-18 23:32:07 -0300

    last commit

--- src/components/product/gallery/GalleryProduct.jsx
+++ src/components/product/gallery/GalleryProduct.jsx
@@ -1,5 +1,6 @@
 import SlideProduct from "@/components/product/gallery/SlideProduct";
 import { useState } from "react";
+import ModalProduct from "./SlideProduct";
 
 export default ({ ARRAY_IMGS, ARRAY_IMG_SMALL }) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
@@ -18,13 +19,17 @@ export default ({ ARRAY_IMGS, ARRAY_IMG_SMALL }) => {
       />
       {isOpenModal && (
         <>
-          <SlideProduct
+          <ModalProduct
             ARRAY_IMGS={ARRAY_IMGS}
             ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
-            className="md:mx-w-md hidden md:absolute md:top-1/2 md:left-1/2 md:grid md:-translate-y-1/2 md:-translate-x-1/2 md:grid-cols-4 md:gap-4"
+            className="md:mx-w-md hidden md:absolute md:top-1/2 md:left-1/2 md:z-10 md:grid md:-translate-y-1/2 md:-translate-x-1/2 md:grid-cols-4 md:gap-4"
             handleCloseModal={handleCloseModal}
             isOpenModal={isOpenModal}
           />
+          <span
+            className="fixed top-0 left-0 h-full w-full bg-black/70 "
+            onClick={handleCloseModal}
+          ></span>
         </>
       )}
     </>
