4f78e071e359057ee0d4f5037366d01695feb293
Alek30k <alemank23k@gmail.com>
2023-01-22 16:45:10 -0300

    last commit

--- src/components/product/details/DetailsProduct.jsx
+++ src/components/product/details/DetailsProduct.jsx
@@ -6,15 +6,12 @@ export default ({ objectProduct }) => {
             <p className="mb-3 font-bold uppercase tracking-wide text-orange-primary">
                 {objectProduct.subtitle}
             </p>
-            <h2 className="mb-4 text-3xl font-bold ">
-                Fall Limit Edition Sneakers
-            </h2>
+            <h2 className="mb-4 text-3xl font-bold ">{objectProduct.title}</h2>
             <p className="mb-5 text-dark-grayish-blue">
                 {objectProduct.description}
             </p>
             <div className="mb-5 grid grid-cols-3 items-center gap-4 font-bold md:grid-cols-[1fr_3fr] md:gap-1">
                 <span className="text-3xl">
-                    {" "}
                     $
                     {(
                         objectProduct.price *
@@ -22,10 +19,10 @@ export default ({ objectProduct }) => {
                     ).toFixed(2)}
                 </span>
                 <span className=" mr-auto rounded-md bg-pale-orange py-1 px-2 text-orange-primary">
-                    50%
+                    {objectProduct.discount * 100}%
                 </span>
                 <span className="text-right text-lg text-grayish-blue line-through md:col-span-2 md:text-left">
-                    $250.00
+                    ${objectProduct.price.toFixed(2)}
                 </span>
             </div>
             <div className="grid grid-cols-3 gap-4 font-bold md:grid-cols-[1fr_1.7fr]">
