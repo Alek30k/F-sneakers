e8febea6d11cf8cc869a4b3b3434b8760aa32851
Alek30k <alemank23k@gmail.com>
2023-01-22 17:07:35 -0300

    last commit

--- src/components/product/IndexProduct.jsx
+++ src/components/product/IndexProduct.jsx
@@ -36,8 +36,8 @@ const MainProduct = () => {
     return (
         <main className="grid grid-cols-1 items-center gap-8 md:container md:mx-auto md:min-h-[calc(100vh-88px-3px)] md:grid-cols-2">
             <GalleryProduct
-                ARRAY_IMGS={ARRAY_IMGS}
-                ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
+                ARRAY_IMGS={objectProduct.imagesMain}
+                ARRAY_IMG_SMALL={objectProduct.imagesSmall}
             />
             <DetailsProduct objectProduct={objectProduct} />
         </main>
