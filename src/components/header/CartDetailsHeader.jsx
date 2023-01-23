a9deeca56e0d4115983d3b29cf87b77f48a65f54
Alek30k <alemank23k@gmail.com>
2023-01-23 15:37:09 -0300

    last commit

--- src/components/header/CartDetailsHeader.jsx
+++ src/components/header/CartDetailsHeader.jsx
@@ -11,20 +11,21 @@ export default () => {
             <div className="mx-4 rounded-md bg-white shadow-md">
                 <h4 className="px-6 py-8 text-lg font-bold">Cart</h4>
                 <hr />
-                <article className="grid grid-cols-[1fr_4fr_1fr] items-center gap-6 px-6 py-4">
-                    <img src={ImgSmall} alt="" className="rounded-md" />
-                    <div>
-                        <h6>Fall Limited Edition Sneakers</h6>
-                        <p>
-                            <span>$125.00 x 3</span>
-                            <span className="font-bold">$375.00</span>
-                        </p>
-                    </div>
-                    <button className="ml-auto">
-                        <DeleteIcon className="hover:fill-orange-primary" />
-                    </button>
-                </article>
-
+                {cartProducts.map((product) => (
+                    <article className="grid grid-cols-[1fr_4fr_1fr] items-center gap-6 px-6 py-4">
+                        <img src={ImgSmall} alt="" className="rounded-md" />
+                        <div>
+                            <h6>Fall Limited Edition Sneakers</h6>
+                            <p>
+                                <span>$125.00 x 3</span>
+                                <span className="font-bold">$375.00</span>
+                            </p>
+                        </div>
+                        <button className="ml-auto">
+                            <DeleteIcon className="hover:fill-orange-primary" />
+                        </button>
+                    </article>
+                ))}
                 <div className="px-6 pb-8">
                     <button className="w-full rounded-md bg-orange-primary py-4 text-white transition-all hover:bg-orange-700">
                         Checkout
--- src/context/useCartDetails.jsx
+++ src/context/useCartDetails.jsx
@@ -2,29 +2,40 @@ import { createContext, useState } from "react";
 export const useCartDetails = createContext();
 
 export default (props) => {
-    const [cartProducts, setCartProducts] = useState([]);
+    const [cartProducts, setCartProducts] = useState([
+        {
+            id: 1,
+            title: "Fall Limited Edition Sneakers",
+            description:
+                "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+            subtitle: "SNEAKER COMPANY",
+            price: 250,
+            discount: 0.45,
+            imagesMain: ARRAY_IMGS,
+            imagesSmall: ARRAY_IMG_SMALL,
+        },
+    ]);
 
     const addCartProducts = (product) => {
-        if (cartProducts.length === 0) {
-            return setCartProducts([...cartProducts, product]);
-        }
-
-        setCartProducts(
-            cartProducts.map((item) => {
-                if (item.id === product.id) {
-                    return {
-                        ...item,
-                        quantity: item.quantity + product.quantity,
-                    };
-                } else {
-                    return item;
-                }
-            })
-        );
+        // if (cartProducts.length === 0) {
+        //     return setCartProducts([...cartProducts, product]);
+        // }
+        // setCartProducts(
+        //     cartProducts.map((item) => {
+        //         if (item.id === product.id) {
+        //             return {
+        //                 ...item,
+        //                 quantity: item.quantity + product.quantity,
+        //             };
+        //         } else {
+        //             return item;
+        //         }
+        //     })
+        // );
     };
 
     const deleteCartProducts = (id) => {
-        setCartProducts(cartProducts.filter((item) => item.id !== id));
+        // setCartProducts(cartProducts.filter((item) => item.id !== id));
     };
 
     // const totalQuantityProduct = cartProducts.reduce(
@@ -37,7 +48,7 @@ export default (props) => {
             value={{
                 cartProducts,
                 addCartProducts,
-                // deleteCartProducts,
+                deleteCartProducts,
                 // totalQuantityProduct,
             }}
         >
