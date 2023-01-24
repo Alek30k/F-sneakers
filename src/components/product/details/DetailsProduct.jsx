d6b352aad081178976623d08ae9f879e914ee7b8
Alek30k <alemank23k@gmail.com>
2023-01-24 15:07:46 -0300

    last commit

--- src/components/product/details/DetailsProduct.jsx
+++ src/components/product/details/DetailsProduct.jsx
@@ -6,6 +6,11 @@ export default ({ objectProduct }) => {
     const { addCartProducts } = useContext(useCartDetails);
     const [count, setCount] = useState(0);
 
+    const decrementCount = () => {
+        if (count === 0) return;
+        setCount(count - 1);
+    };
+
     const handleAddToCart = () => {
         addCartProducts({
             img: objectProduct.imagesSmall[0],
@@ -46,9 +51,19 @@ export default ({ objectProduct }) => {
             </div>
             <div className="grid grid-cols-3 gap-4 font-bold md:grid-cols-[1fr_1.7fr]">
                 <div className="col-span-3 flex items-baseline justify-between rounded-md bg-gray-200 py-2 px-5 pb-3 md:col-span-1">
-                    <button className="text-3xl text-orange-primary">-</button>
+                    <button
+                        className="text-3xl text-orange-primary"
+                        onClick={decrementCount}
+                    >
+                        -
+                    </button>
                     <span className="text-xl">0</span>
-                    <button className="text-3xl text-orange-primary">+</button>
+                    <button
+                        className="text-3xl text-orange-primary"
+                        onClick={() => setCount(count + 1)}
+                    >
+                        +
+                    </button>
                 </div>
                 <button
                     className="col-span-3 flex items-center justify-center gap-x-3 rounded-md bg-orange-primary py-3 text-white transition-all hover:bg-orange-700 md:col-span-1"
